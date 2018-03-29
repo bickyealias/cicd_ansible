0b29b6065dc8e9dc000ea5905639d7e02c10030e
EC2 Default User <ec2-user@ip-172-31-25-118.ap-south-1.compute.internal>
2018-03-29 14:37:27 +0000

    todosetup

--- src/todobackend/settings.py
+++ src/todobackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+5j1bo(2nk(de*gn-hc%r%cpbl5t_=u(-k=mm%pe$qjmhhp7wh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.126.237.183','localhost']
 
 
 # Application definition
@@ -37,11 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'todo'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#CORS settings
+
+CORS_ORIGIN_ALLOW_ALL = True
